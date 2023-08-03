99db68700785365b978627a9b84a70d90bd37c30
Martín <magadiflo@gmail.com>
2023-08-03 13:09:59 -0500

    Standalone - Como Página

---
+++ src/app/alone/pages/alone-page/alone-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-alone-page',
+  standalone: true,
+  imports: [NgIf],
+  templateUrl: './alone-page.component.html',
+  styleUrls: ['./alone-page.component.css']
+})
+export class AlonePageComponent {
+
+}
