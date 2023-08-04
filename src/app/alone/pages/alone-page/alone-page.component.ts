f8843518d70834818668da984f4235d4ea2b55ae
Martín <magadiflo@gmail.com>
2023-08-04 10:17:02 -0500

    Standalone - Cómo componente reutilizable

--- src/app/alone/pages/alone-page/alone-page.component.ts
+++ src/app/alone/pages/alone-page/alone-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { NgIf } from '@angular/common';
 
+import { CounterAloneComponent } from '../../components/counter-alone/counter-alone.component';
+
 @Component({
   selector: 'app-alone-page',
   standalone: true,
-  imports: [NgIf],
+  imports: [NgIf, CounterAloneComponent],
   templateUrl: './alone-page.component.html',
   styleUrls: ['./alone-page.component.css']
 })
