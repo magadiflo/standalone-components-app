e7d6988b7d1ec959ffd60ec7eb4588ace377be18
Martín <magadiflo@gmail.com>
2023-08-04 10:34:50 -0500

    Standalone - en módulos tradicionales

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -13,6 +13,9 @@ import { MarkersPageComponent } from './pages/markers-page/markers-page.componen
 import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
 import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.component';
 
+//* standalone component
+import { CounterAloneComponent } from '../alone/components/counter-alone/counter-alone.component';
+
 
 @NgModule({
   declarations: [
@@ -26,7 +29,8 @@ import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.
   ],
   imports: [
     CommonModule,
-    MapsRoutingModule
+    MapsRoutingModule,
+    CounterAloneComponent,
   ]
 })
 export class MapsModule { }
